230cc6b82e88d3b07fef010a53f88d6b3c2ca4ea
varshitk7 <129107427+varshitk7@users.noreply.github.com>
2024-03-12 23:07:24 +0530

    Create Sorting.java

---
+++ Sorting.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Sorting{  
+    static void sort(int[] arr, int n){  
+        int temp = 0;  
+        for(int i = 0; i < n; i++){  
+            for(int j = 1; j < n - i; j++){  
+                if(arr[j - 1] > arr[j]){  
+                    temp = arr[j-1];  
+                    arr[j - 1] = arr[j];  
+                    arr[j] = temp;  
+                }  
+            }  
+        }  
+    }  
+    public static void main(String[] args) {  
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Array Size :");
+        int n = sc.nextInt();
+        System.out.println("Enter Array Elements :");
+        int arr[] = new int[n+1];
+        for(int i = 0;i < n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter a number :");
+        int num = sc.nextInt();
+        sort(arr, n);
+        System.out.println("Sorted Array :");
+        for(int i= 0;i < n;i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        int count = 0;
+        for(int i = 0;i < n;i++){
+            if(arr[i] > num)
+            break;
+            if(arr[i] == num)
+            count += 1;
+        }
+        System.out.println("Number of occurrences : " + count);
+    }
+    
+}
